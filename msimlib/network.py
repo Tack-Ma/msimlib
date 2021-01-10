854bd023182f3252025f540cbd87e7df73379120
Tack-Ma <schecteer0523@gmail.com>
2021-01-11 00:57:40 +0900

    add connection

--- msimlib/network.py
+++ msimlib/network.py
@@ -16,8 +16,20 @@ class NetModel:
     def nodes_count(self):
         return len(self.values)
 
-    def connect(self):
-        pass
+    @property
+    def edge_count(self):
+        return len(self.relation)
+
+    @property
+    def relation_array(self):
+        arr = numpy.zeros((self.edge_count, self.nodes_count))
+        for i, relation in enumerate(self.relation):
+            arr[i][relation[0]] = -1
+            arr[i][relation[1]] = 1
+        return arr
+
+    def connect(self, relation: list):
+        self.relation.append(relation)
 
     def concatenate(self):
         pass
